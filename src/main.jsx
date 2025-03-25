c091dab7af5fbdca7c450cc85544f26080fc0971
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-03-25 12:31:11 +0200

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,14 @@ import App from './App.jsx'
 const name = "Rajon Al Saeid";
 const first_page = 2023;
 
-const demo = (
-  <div>
-    <h1>Hello! My name is {name}</h1>
-    <p>I have {2025 - first_page} years of experience in web development</p>
-  </div>
-);
-
+const Demo= () => {
+  return (
+    <div>
+      <h1>Hello! My name is {name}</h1>
+      <p>I have {2025 - first_page} years of experience in web development</p>
+    </div>
+  );
+};
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   {demo}
-  </StrictMode>,
-)
+  <StrictMode> {Demo} </StrictMode>,
+) 
